10b4159dd910a0200d05392c8447f3ce5eda66a6
daveh6475 <151473718+daveh6475@users.noreply.github.com>
2024-07-05 23:24:40 +0100

    Update main.py

--- src/main.py
+++ src/main.py
@@ -7,35 +7,19 @@ import base64
 import cProfile
 import pstats
 import io
-from datetime import datetime, date
+
+from datetime import datetime
 from PIL import Image, ImageDraw, ImageFont
 from PIL.ImageFont import FreeTypeFont
-from dataclasses import dataclass
-from typing import Any, List, Dict, Tuple
+from helpers import get_device, AnimatedObject, RenderText, Animation, AnimationSequence, move_object, scroll_left, scroll_up, ObjectRow, reset_object
+from trains import loadDeparturesForStationRTT, loadDestinationsForDepartureRTT, ProcessedDepartures, CallingPoints
 from luma.core.render import canvas
 from luma.core.virtual import viewport, snapshot
-from helpers import get_device, AnimatedObject, RenderText, Animation, AnimationSequence, move_object, scroll_left, scroll_up, ObjectRow, reset_object
 from open import isRun
+from typing import Any, Dict, List, Tuple
 
 DISPLAY_WIDTH = 256
 DISPLAY_HEIGHT = 64
-
-@dataclass
-class ProcessedDepartures:
-    uid: str
-    destination_name: str
-    aimed_departure_time: str
-    expected_departure_time: str
-    status: str
-    mode: str
-    platform: str
-    timetable_url: str
-    toc: str
-
-@dataclass
-class CallingPoints:
-    station: str
-    arrival_time: str
 
 def loadConfig() -> Dict[str, Any]:
     with open('config.json', 'r') as jsonConfig:
@@ -55,113 +39,55 @@ def makeFont(name: str, size: int) -> FreeTypeFont:
 def format_hhmm(timestamp: str) -> str:
     return f"{timestamp[0:2]}:{timestamp[2:4]}"
 
-def abbrStation(journeyConfig: Dict[str, Any], inputStr: str) -> str:
-    abbreviations = journeyConfig['stationAbbr']
-    for key, value in abbreviations.items():
-        inputStr = inputStr.replace(key, value)
-    return inputStr
-
-def loadDeparturesForStationRTT(journeyConfig: Dict[str, Any], username: str, password: str) -> Tuple[List[ProcessedDepartures], str]:
-    if not journeyConfig.get("departureStation"):
-        raise ValueError("Please set the journey.departureStation property in config.json")
-
-    if not username or not password:
-        raise ValueError("Please complete the rttApi section of your config.json file")
-
-    departureStation = journeyConfig["departureStation"]
-    response = requests.get(f"https://api.rtt.io/api/v1/json/search/{departureStation}", auth=(username, password))
-    response.raise_for_status()
-    data = response.json()
-    translated_departures = []
-    td = date.today()
-
-    services = data.get('services', [])
-    if not services:
-        return translated_departures, departureStation
-
-    for item in services[:5]:
-        uid = item['serviceUid']
-        destination_name = abbrStation(journeyConfig, item['locationDetail']['destination'][0]['description'])
-
-        dt = item['locationDetail']['gbttBookedDeparture']
-        edt = item['locationDetail'].get('realtimeDeparture', dt)
-
-        aimed_departure_time = f"{dt[:2]}:{dt[2:]}"
-        expected_departure_time = f"{edt[:2]}:{edt[2:]}"
-        status = item['locationDetail']['displayAs']
-        mode = item['serviceType']
-        platform = item['locationDetail'].get('platform', "")
-        toc = item["atocName"]
-
-        translated_departures.append(
-            ProcessedDepartures(
-                uid=uid, destination_name=abbrStation(journeyConfig, destination_name), aimed_departure_time=aimed_departure_time,
-                expected_departure_time=expected_departure_time, status=status, mode=mode, platform=platform,
-                timetable_url=f"https://api.rtt.io/api/v1/json/service/{uid}/{td.year}/{td.month:02}/{td.day:02}",
-                toc=toc
-            )
-        )
-
-    return translated_departures, departureStation
-
-def loadDestinationsForDepartureRTT(journeyConfig: Dict[str, Any], username: str, password: str, timetableUrl: str) -> List[CallingPoints]:
-    r = requests.get(url=timetableUrl, auth=(username, password))
-    r.raise_for_status()
-    calling_data = r.json()
-
-    index = next((i for i, loc in enumerate(calling_data['locations']) if loc['crs'] == journeyConfig["departureStation"]), None)
-    if index is None:
-        return []
-
-    calling_at = [
-        CallingPoints(abbrStation(journeyConfig, loc['description']), loc["realtimeArrival"])
-        for loc in calling_data['locations'][index+1:]
-    ]
-
-    return calling_at
-
-def renderText(draw, width: int, height: int, text: str, font: FreeTypeFont, position: Tuple[int, int], fill: str = "yellow"):
-    draw.text(position, text=text, font=font, fill=fill)
+def renderText(draw, width: int, height: int, text: str, font: FreeTypeFont, position: Tuple[int, int]):
+    draw.text(position, text=text, font=font, fill="yellow")
 
 def renderDestination(departure: Dict[str, Any], font: FreeTypeFont, n: int = 0):
     departureTime = departure['departureTime']
     destinationName = departure['destination']
 
-    ordinal = ""
-    if n == 1:
-        ordinal = "1st "
-    elif n == 2:
-        ordinal = "2nd "
-    elif n == 3:
-        ordinal = "3rd "
-    elif n == 4:
-        ordinal = "4th "
-
-    train = f"{ordinal}{departureTime}  {destinationName}"
-
     def drawText(draw, width: int, height: int):
+        ordinal = ""
+        if n == 1:
+            ordinal = "1st "
+        elif n == 2:
+            ordinal = "2nd "
+        elif n == 3:
+            ordinal = "3rd "
+        elif n == 4:
+            ordinal = "4th "
+
+        train = f"{ordinal}{departureTime}  {destinationName}"
         renderText(draw, width, height, train, font, (0, 0))
 
     return drawText
 
-def renderServiceStatus(departure: Dict[str, Any]):
+def renderServiceStatus(departure: Dict[str, Any], font: FreeTypeFont):
     def drawText(draw, width: int, height: int):
-        train = "Cancelled" if departure['isCancelled'] else 'Exp ' + departure['departureTime']
+        train = ""
+
+        if departure['isCancelled']:
+            train = "Cancelled"
+        else:
+            train = 'Exp ' + departure['departureTime']
+
         w = int(draw.textlength(train, font))
         renderText(draw, width, height, train, font, (width - w, 0))
+
     return drawText
 
-def renderPlatform(departure: Dict[str, Any]):
+def renderPlatform(departure: Dict[str, Any], font: FreeTypeFont):
     def drawText(draw, width: int, height: int):
-        if not departure['isCancelled'] and isinstance(departure['platform'], str):
-            renderText(draw, width, height, "Plat " + departure['platform'], font, (0, 0))
+        if not departure['isCancelled']:
+            if isinstance(departure['platform'], str):
+                renderText(draw, width, height, "Plat " + departure['platform'], font, (0, 0))
     return drawText
 
-def renderCallingAt(draw, width: int, height: int):
+def renderCallingAt(draw, width: int, height: int, font: FreeTypeFont):
     renderText(draw, width, height, "Calling at:", font, (0, 0))
 
 def get_stations_string(stations: List[CallingPoints], toc: str) -> str:
-    if not stations:
+    if not stations:  # Check if stations list is empty
         return "No calling points available."
 
     if len(stations) == 1:
@@ -173,7 +99,7 @@ def get_stations_string(stations: List[CallingPoints], toc: str) -> str:
     calling_at_str += f"    (A {toc} service.)"
     return calling_at_str
 
-def renderStations(stations: List[CallingPoints], toc: str):
+def renderStations(stations: List[CallingPoints], toc: str, font: FreeTypeFont):
     calling_at_str = get_stations_string(stations, toc)
 
     def drawText(draw, width: int, height: int):
@@ -195,27 +121,29 @@ def renderStations(stations: List[CallingPoints], toc: str):
 
     return drawText
 
-def renderTime(draw, width: int, height: int):
+def renderTime(draw, width: int, height: int, fontBoldLarge: FreeTypeFont, fontBoldTall: FreeTypeFont):
     rawTime = datetime.now().time()
     hour, minute, second = str(rawTime).split('.')[0].split(':')
 
-    w1 = int(draw.textlength(f"{hour}:{minute}", fontBoldLarge))
+    w1 = int(draw.textlength("{}:{}".format(hour, minute), fontBoldLarge))
     w2 = int(draw.textlength(":00", fontBoldTall))
 
-    renderText(draw, width, height, f"{hour}:{minute}", fontBoldLarge, ((width - w1 - w2) / 2, 0))
-    renderText(draw, width, height, f":{second}", fontBoldTall, (((width - w1 - w2) / 2) + w1, 5))
+    renderText(draw, width, height, "{}:{}".format(hour, minute), fontBoldLarge, ((width - w1 - w2) / 2, 0))
+    renderText(draw, width, height, ":{}".format(second), fontBoldTall, (((width - w1 - w2) / 2) + w1, 5))
 
-def renderWelcomeTo(xOffset: int):
+def renderWelcomeTo(xOffset: int, fontBold: FreeTypeFont):
     def drawText(draw, width: int, height: int):
         renderText(draw, width, height, "Welcome to", fontBold, (int(xOffset), 0))
+
     return drawText
 
-def renderDepartureStation(departureStation: str, xOffset: int):
+def renderDepartureStation(departureStation: str, xOffset: int, fontBold: FreeTypeFont):
     def draw(draw, width: int, height: int):
         renderText(draw, width, height, departureStation, fontBold, (int(xOffset), 0))
+
     return draw
 
-def renderDots(draw, width: int, height: int):
+def renderDots(draw, width: int, height: int, fontBold: FreeTypeFont):
     renderText(draw, width, height, ".  .  .", fontBold, (0, 0))
 
 def loadDataRTT(apiDetails: Dict[str, Any], journey: Dict[str, Any]) -> Tuple[List[Dict[str, Any]], List[CallingPoints], str]:
@@ -271,7 +199,7 @@ def loadDataRTT(apiDetails: Dict[str, Any], journey: Dict[str, Any]) -> Tuple[Li
 
     return departures, first_departure_destinations, journey['departureStation']
 
-def drawBlankSignage(device, width: int, height: int, departureStation: str):
+def drawBlankSignage(device, width: int, height: int, departureStation: str, fontBold: FreeTypeFont, fontBoldLarge: FreeTypeFont):
     global stationRenderCount, pauseCount
 
     with canvas(device) as draw:
@@ -287,11 +215,11 @@ def drawBlankSignage(device, width: int, height: int, departureStation: str):
     virtualViewport = viewport(device, width=width, height=height)
 
     rowOne = snapshot(width, 10, renderWelcomeTo(
-        int((width - welcomeSizeX) / 2)), interval=10)
+        int((width - welcomeSizeX) / 2), fontBold), interval=10)
     rowTwo = snapshot(width, 10, renderDepartureStation(
-        departureStation, int((width - stationSizeX) / 2)), interval=10)
-    rowThree = snapshot(width, 10, renderDots, interval=10)
-    rowTime = snapshot(width, 14, renderTime, interval=1)
+        departureStation, int((width - stationSizeX) / 2), fontBold), interval=10)
+    rowThree = snapshot(width, 10, renderDots, interval=10, fontBold=fontBold)
+    rowTime = snapshot(width, 14, renderTime, interval=1, fontBoldLarge=fontBoldLarge, fontBoldTall=fontBoldTall)
 
     if len(virtualViewport._hotspots) > 0:
         for hotspot, xy in virtualViewport._hotspots:
@@ -304,7 +232,7 @@ def drawBlankSignage(device, width: int, height: int, departureStation: str):
 
     return virtualViewport
 
-def drawSignage(device, width, height, data, font, fontBold):
+def drawSignage(device, width, height, data, font, fontBold, fontBoldLarge, fontBoldTall):
     global stationRenderCount, pauseCount
 
     device.clear()
@@ -331,26 +259,26 @@ def drawSignage(device, width, height, data, font, fontBold):
     rowOneA = snapshot(
         width - w - pw, 10, renderDestination(departures[0], fontBold), interval=10)
     rowOneB = snapshot(w, 10, renderServiceStatus(
-        departures[0]), interval=1)
-    rowOneC = snapshot(pw, 10, renderPlatform(departures[0]), interval=10)
-    rowTwoA = snapshot(callingWidth, 10, renderCallingAt, interval=100)
+        departures[0], font), interval=1)
+    rowOneC = snapshot(pw, 10, renderPlatform(departures[0], font), interval=10)
+    rowTwoA = snapshot(callingWidth, 10, renderCallingAt, interval=100, font=font)
     rowTwoB = snapshot(width - callingWidth, 10,
-                       renderStations(firstDepartureDestinations, departures[0]['atocName']), interval=0.1)
+                       renderStations(firstDepartureDestinations, departures[0]['atocName'], font), interval=0.1)
     if len(departures) > 1:
         rowThreeA = snapshot(width - w - pw, 10, renderDestination(
             departures[1], font), interval=10)
         rowThreeB = snapshot(w, 10, renderServiceStatus(
-            departures[1]), interval=1)
-        rowThreeC = snapshot(pw, 10, renderPlatform(departures[1]), interval=10)
+            departures[1], font), interval=1)
+        rowThreeC = snapshot(pw, 10, renderPlatform(departures[1], font), interval=10)
 
     if len(departures) > 2:
         rowFourA = snapshot(width - w - pw, 10, renderDestination(
             departures[2], font), interval=10)
         rowFourB = snapshot(w, 10, renderServiceStatus(
-            departures[2]), interval=1)
-        rowFourC = snapshot(pw, 10, renderPlatform(departures[2]), interval=10)
+            departures[2], font), interval=1)
+        rowFourC = snapshot(pw, 10, renderPlatform(departures[2], font), interval=10)
 
-    rowTime = snapshot(width, 14, renderTime, interval=1)
+    rowTime = snapshot(width, 14, renderTime, interval=1, fontBoldLarge=fontBoldLarge, fontBoldTall=fontBoldTall)
 
     if len(virtualViewport._hotspots) > 0:
         for hotspot, xy in virtualViewport._hotspots:
@@ -385,7 +313,6 @@ def main():
     fontBoldTall = makeFont("Dot Matrix Bold Tall.ttf", 10)
     fontBoldLarge = makeFont("Dot Matrix Bold.ttf", 20)
 
-    global stationRenderCount, pauseCount
     stationRenderCount = 0
     pauseCount = 0
     loop_count = 0
@@ -397,10 +324,10 @@ def main():
 
     if len(data[0]) == 0:
         virtual = drawBlankSignage(
-            device, width=DISPLAY_WIDTH, height=DISPLAY_HEIGHT, departureStation=data[2])
+            device, width=DISPLAY_WIDTH, height=DISPLAY_HEIGHT, departureStation=data[2], fontBold=fontBold, fontBoldLarge=fontBoldLarge)
     else:
         virtual = drawSignage(device, width=DISPLAY_WIDTH,
-                              height=DISPLAY_HEIGHT, data=data, font=font, fontBold=fontBold)
+                              height=DISPLAY_HEIGHT, data=data, font=font, fontBold=fontBold, fontBoldLarge=fontBoldLarge, fontBoldTall=fontBoldTall)
 
     timeAtStart = time.time()
     timeNow = time.time()
@@ -412,10 +339,10 @@ def main():
 
             if len(data[0]) == 0:
                 virtual = drawBlankSignage(
-                    device, width=DISPLAY_WIDTH, height=DISPLAY_HEIGHT, departureStation=data[2])
+                    device, width=DISPLAY_WIDTH, height=DISPLAY_HEIGHT, departureStation=data[2], fontBold=fontBold, fontBoldLarge=fontBoldLarge)
             else:
                 virtual = drawSignage(device, width=DISPLAY_WIDTH,
-                                      height=DISPLAY_HEIGHT, data=data, font=font, fontBold=fontBold)
+                                      height=DISPLAY_HEIGHT, data=data, font=font, fontBold=fontBold, fontBoldLarge=fontBoldLarge, fontBoldTall=fontBoldTall)
 
             timeAtStart = time.time()
 
