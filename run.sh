9514c559434ef70138dfbd1af8c28fd11414854a
daveh6475 <151473718+daveh6475@users.noreply.github.com>
2024-07-12 10:29:55 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,18 +1,3 @@
 #!/bin/bash
 
-# Check if config.json exists, if not, initialize an empty JSON file
-if [ ! -f config.json ]; then
-  echo '{}' > config.json
-fi
-
-# Update values in config.json
-jq .journey.departureStation=\""${departureStation}"\" config.json | sponge config.json
-jq .journey.destinationStation=\""${destinationStation}"\" config.json | sponge config.json
-jq .journey.timeOffset=\""${timeOffset}"\" config.json | sponge config.json
-jq .journey.outOfHoursName=\""${outOfHoursName}"\" config.json | sponge config.json
-jq .refreshTime="${refreshTime}" config.json | sponge config.json
-jq .transportApi.apiKey=\""${transportApi_apiKey}"\" config.json | sponge config.json
-jq .transportApi.operatingHours=\""${transportApi_operatingHours}"\" config.json | sponge config.json
-
-# Run the main Python script with display parameters
 python3 ./src/main.py --display ssd1322 --width 256 --height 64 --interface spi --mode 1 --rotate 2
